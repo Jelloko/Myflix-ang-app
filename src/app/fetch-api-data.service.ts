2c30b30b552ce134a7fee876d54ced9d88257efc
Jelloko <rainbow.samp@gmail.com>
2025-01-07 00:45:47 -0500

    This has all the paths for the backend and directly fetches all the information used in this app from the api

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -3,6 +3,7 @@ import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 //Declaring the api url that will provide data for the client app
 const apiUrl = 'https://my-flix-cf-fd6a3633859c.herokuapp.com/';
@@ -12,16 +13,31 @@ const apiUrl = 'https://my-flix-cf-fd6a3633859c.herokuapp.com/';
 export class UserRegistrationService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
-  constructor(private http: HttpClient) {
+  constructor(public http: HttpClient, private snackBar: MatSnackBar) {
   }
 
-  private getStoredToken(): any {
-    return localStorage.getItem('token');
+// Function to get the token
+public getStoredToken(): any {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    this.snackBar.open('Authentication token is missing.', 'OK', {
+      duration: 3000,
+    });
   }
+  console.log('Stored Token:', token); // Debugging line
+  return token;
+}
 
-  private getStoredUser(): any {
-    return localStorage.getItem('user');
+// Function to get the user
+public getStoredUser(): any {
+  const user = localStorage.getItem('currentUser');
+  if (!user) {
+    this.snackBar.open('User information is missing.', 'OK', { duration: 3000 });
   }
+  console.log('Stored User:', user); // Debugging line
+  return user ? JSON.parse(user) : null; // Parse JSON here
+}
+
 
  // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
@@ -93,6 +109,20 @@ export class UserRegistrationService {
     );
   }
 
+  // Get a single user by name
+public getUserByName(name: string) {
+  const token = this.getStoredToken();
+  return this.http.get(apiUrl + 'users/' + name, {
+    headers: new HttpHeaders({
+      Authorization: 'Bearer ' + token,
+    })
+  }).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+
+
   // Get all users
   public getAllUsers() {
     const token = this.getStoredToken();
@@ -107,26 +137,36 @@ export class UserRegistrationService {
     );
   }
 
-  // Add a movie to favourite Movies
-  public addUserFavoriteMovie(movieId: string) {
-    const token = this.getStoredToken();
-    let user = this.getStoredUser();
-    return this.http.put(apiUrl + 'users/' + user.name + '/FavoriteMovies/' + movieId, {
-      headers: new HttpHeaders(
-        {
-          Authorization: 'Bearer ' + token,
-        })
-    }).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+  // Add a movie to favorite Movies
+public addFavoriteMovie(moviesId: string): Observable<any> {
+  const token = this.getStoredToken();
+  const user = this.getStoredUser();  // This returns the full user object
+
+  if (!token || !user || !user.Name) {
+    console.error('Authentication token or user is missing.');
+    return throwError(() => new Error('Authentication token or user is missing.'));
   }
 
+  return this.http.put(
+    `${apiUrl}users/${user.Name}/FavoriteMovies/${moviesId}`,  // Use user.Name here
+    {},  // You can pass an empty object if no body content is required
+    {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`,
+      }),
+    }
+  ).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+
+
   // Delete a movie from favourite Movies
-  public deleteUserFavoriteMovie(movieId: string) {
+  public deleteFavoriteMovie(moviesId: string) {
     const token = this.getStoredToken();
     let user = this.getStoredUser();
-    return this.http.delete(apiUrl + 'users/' + user.name + '/FavoriteMovies/' + movieId, {
+    return this.http.delete(apiUrl + 'users/' + user.name + '/FavoriteMovies/' + moviesId, {
       headers: new HttpHeaders(
         {
           Authorization: 'Bearer ' + token,
@@ -141,7 +181,7 @@ export class UserRegistrationService {
   public editUser(userDetails: any): Observable<any> {
     const token = this.getStoredToken();
     let user = this.getStoredUser();
-    return this.http.put(apiUrl + 'users/' + user.name, userDetails, {
+    return this.http.put(apiUrl + 'users/' + user.Name, userDetails, {
       headers: new HttpHeaders(
         {
           "Content-Type": "application/json",
@@ -157,7 +197,7 @@ export class UserRegistrationService {
   public deleteUser(userDetails: any): Observable<any> {
     const token = this.getStoredToken();
     let user = this.getStoredUser();
-    return this.http.delete(apiUrl + 'users/' + user.name, {
+    return this.http.delete(apiUrl + 'users/' + user.name,  {
       headers: new HttpHeaders(
         {
           Authorization: 'Bearer ' + token,
@@ -168,7 +208,7 @@ export class UserRegistrationService {
     );
   }
 
-private handleError(error: HttpErrorResponse): any {
+public handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
     console.error('Some error occurred:', error.error.message);
     } else {
@@ -181,7 +221,7 @@ private handleError(error: HttpErrorResponse): any {
   }
 
   // Non-typed response extraction
-  private extractResponseData(res: any): any {
+  public extractResponseData(res: any): any {
     const body = res;
     return body || {};
   }
